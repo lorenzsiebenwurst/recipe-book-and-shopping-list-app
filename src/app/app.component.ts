994aaabe1039e23887845fb73cde4118c9565cf0
lorenzSiebenwurst <lo@ohn-line.de>
2022-07-13 14:52:07 +0200

    initial commit with componentsbase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'recipe-book-and-shopping-list-app';
-}
+export class AppComponent {}
