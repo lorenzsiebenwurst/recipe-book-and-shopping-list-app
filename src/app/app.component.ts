6e38dfd26c83a29a9d9ed9d24ad8a196f1b3e0c7
lorenzSiebenwurst <lo@ohn-line.de>
2022-07-18 17:10:29 +0200

    completed the next excercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  topic: string = 'recipes';
+
+  switchTopic(topic: string) {
+    this.topic = topic;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() selectedTopic: EventEmitter<string> = new EventEmitter();
+  topic: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectRecipesView() {
+    this.topic = 'recipes';
+    this.selectedTopic.emit(this.topic);
   }
 
+  selectShoppingListView() {
+    this.topic = 'shopping-list';
+    this.selectedTopic.emit(this.topic);
+  }
 }
