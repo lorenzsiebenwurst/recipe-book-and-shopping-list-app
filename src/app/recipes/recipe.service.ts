13a398ae925e7fa0177cccec2c020a086955ef0e
lorenzSiebenwurst <lo@ohn-line.de>
2022-07-20 12:26:38 +0200

    new services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,33 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
-
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg'
+      'Something like a Schnitzl',
+      'Try it - Love it!',
+      'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply a second test',
-      'https://www.oreilly.com/content/wp-content/uploads/sites/2/2020/01/6297898421_e1abeb3d98_o_crop-b932fa6bb757d4ecc1fa46e6534dc322.jpg'
+      'Grilling a Squash',
+      'I love it!',
+      'https://www.oreilly.com/content/wp-content/uploads/sites/2/2020/01/6297898421_e1abeb3d98_o_crop-b932fa6bb757d4ecc1fa46e6534dc322.jpg',
+      [new Ingredient('Squash', 1), new Ingredient('Potatoes', 4)]
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredient: Ingredient[]) {
+    this.slService.addIngredients(ingredient);
   }
 }
